cff58b0da9b15df6fc6be799c08dae9a38daadd4
julielima8 <julie11@illinois.edu>
2023-02-21 19:30:05 -0600

    added Person and RoommatePair classes

---
+++ RoommatePair.java
@@ -0,0 +1,9 @@
+class RoommatePair {
+    Person roommate1;
+    Person roommate2;
+
+    public RoommatePair(Person one, Person two) {
+        roommate1 = one;
+        roommate2 = two;
+    }
+};
