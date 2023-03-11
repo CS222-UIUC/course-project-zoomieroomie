be6d9b102b0896cbe258ace997a461026fd7c698
vineethagrl <vgurr4@illinois.edu>
2023-03-10 20:31:58 -0600

    adding changes for implementation of login page front end. along with installing react and its corresponding files.

---
+++ ZoomieRoomie/my-app/public/Login.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Submitted: ${username} - ${password}`);
+    // TODO: Implement login logic here
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default LoginPage;
