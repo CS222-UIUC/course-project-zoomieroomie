4c91e436255bb6b1b7615d04c124658a71129fb6
vineethagrl <vgurr4@illinois.edu>
2023-03-30 17:30:43 -0500

    Added changes to Login.tsx and added changes to models.py

--- main/frontend/zoomie-roomie/src/components/Register.tsx
+++ main/frontend/zoomie-roomie/src/components/Register.tsx
@@ -1,14 +1,21 @@
-// Register.tsx
-
 import React, { useState } from 'react';
 
 interface RegisterFormData {
+  firstName: string;
+  lastName: string;
   username: string;
   password: string;
+  confirmPassword: string;
 }
 
 const Register: React.FC = () => {
-  const [formData, setFormData] = useState<RegisterFormData>({ username: '', password: '' });
+  const [formData, setFormData] = useState<RegisterFormData>({
+    firstName: '',
+    lastName: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+  });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -17,6 +24,20 @@ const Register: React.FC = () => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    const emailPattern = /^[^\s@]+@illinois\.edu$/;
+    if (!emailPattern.test(formData.username)) {
+      alert('Please enter a valid @illinois.edu email address.');
+      return;
+    }
+    const passwordPattern = /^(?=.*[A-Z].*[A-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])(?=.{10,})/;
+    if (!passwordPattern.test(formData.password)) {
+      alert('Please enter a password that is at least 10 characters long and contains at least 2 capital letters, at least 1 number, and at least 1 special character from the list: "!,@,#,$<%,&,*".');
+      return;
+    }
+    if (formData.password !== formData.confirmPassword) {
+      alert('Passwords do not match.');
+      return;
+    }
     try {
       const response = await fetch('http://127.0.0.1:5000/register', {
         method: 'POST',
@@ -40,6 +61,14 @@ const Register: React.FC = () => {
       <h1>Register</h1>
       <form onSubmit={handleSubmit}>
         <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange} />
+        </div>
+        <div>
           <label htmlFor="username">Username:</label>
           <input type="text" id="username" name="username" value={formData.username} onChange={handleChange} />
         </div>
@@ -47,10 +76,14 @@ const Register: React.FC = () => {
           <label htmlFor="password">Password:</label>
           <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} />
         </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input type="password" id="confirmPassword" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} />
+        </div>
         <button type="submit">Register</button>
       </form>
     </div>
   );
 };
 
-export default Register
+export default Register;
