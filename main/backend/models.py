0370430e6be0c3c41a54aa60a333daca4b57214d
julielima8 <julie11@illinois.edu>
2023-03-31 11:37:14 -0500

    updated registration, form submission, and linting

--- main/backend/models.py
+++ main/backend/models.py
@@ -1,41 +1,59 @@
-from . import app, db
-from flask_bcrypt import Bcrypt
-from flask_sqlalchemy import SQLAlchemy
-from SQLAlchemy import Column, ForeignKey
-from SQLAlchemy.orm import relationship, backref
-bcrypt = Bcrypt(app)
+"""Database models for ZoomieRoomie"""
+import bcrypt
+from SQLAlchemy import ForeignKey
+from SQLAlchemy.orm import relationship
+from . import db
+
+salt = bcrypt.gensalt()
+# bcrypt = Bcrypt(app)
+
 
 class User(db.Model):
-    __tablename__ = 'users'
+    """User table in database"""
+
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
     firstname = db.Column(db.String(50), nullable=False)
     lastname = db.Column(db.String(50), nullable=False)
-    preferences = relationship('Preferences', uselist=False, backref='user')
+    preferences = relationship("Preferences", uselist=False, backref="user")
 
     def __repr__(self):
-        return f'<User {self.username}>'
-    
+        """Defines string representation"""
+        return f"<User {self.username}>"
+
     def set_password(self, password):
-        self.password = bcrypt.generate_password_hash(
-            password.encode('utf-8')
-        )
+        """Sets user password"""
+        self.password = bcrypt.hashpw(password, salt)
 
     def check_password(self, password):
-        return bcrypt.check_password_hash(self.password, password)
+        """Compares password with hash"""
+        hash_p = bcrypt.hashpw(password, salt)
+        return hash_p == self.password
+
 
 class Preferences(db.Model):
-    __tablename__ == 'preferences'
-    id = db.Column(db.Integer, primary_key=True, ForeignKey('users.id'))
-    l-smoker = db.Column(db.String(50))
+    """Preferences table in database (one-to-one with Users)"""
+
+    __tablename__ = "preferences"
+    id = db.Column(db.Integer, ForeignKey("users.id"), primary_key=True)
+    l_smoker = db.Column(db.String(50))
     smoke = db.Column(db.String(50))
-    l-drinker = db.Column(db.String(50))
+    l_drinker = db.Column(db.String(50))
     drink = db.Column(db.String(50))
     extrovert = db.Column(db.Integer)
-    l-extrovert = db.Column(db.Integer)
-    l-sexuality = db.Column(db.String(50))
+    l_extrovert = db.Column(db.Integer)
+    l_sexuality = db.Column(db.String(50))
     sexuality = db.Column(db.String(50))
-    l-gender = db.Column(db.String(50))
+    l_gender = db.Column(db.String(50))
     gender = db.Column(db.String(50))
-    user = relationship('User', backref='preference')
+    user = relationship("User", backref="preference")
+
+    def __repr__(self):
+        """Defines string representation"""
+        return f"<Preferences for {self.user}>"
+
+    def get_user(self):
+        """Returns user who owns preferences"""
+        return self.user
