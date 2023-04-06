012d3f07bfb26b6267a39f03359bcadcf09be76d
vineethagrl <vgurr4@illinois.edu>
2023-04-06 13:03:57 -0500

    Addint the Register.css file along with additional checks in the Register.tsx file

--- main/backend/models.py
+++ main/backend/models.py
@@ -1,12 +1,15 @@
 from . import app, db
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+
 bcrypt = Bcrypt(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(50), nullable=False)
+    lastName = db.Column(db.String(50), nullable=False)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -18,4 +21,3 @@ class User(db.Model):
 
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password, password)
-
--- main/frontend/zoomie-roomie/src/components/Register.tsx
+++ main/frontend/zoomie-roomie/src/components/Register.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "../css/Register.css";
 
 interface RegisterFormData {
   firstName: string;
@@ -10,11 +11,11 @@ interface RegisterFormData {
 
 const Register: React.FC = () => {
   const [formData, setFormData] = useState<RegisterFormData>({
-    firstName: '',
-    lastName: '',
-    username: '',
-    password: '',
-    confirmPassword: '',
+    firstName: "",
+    lastName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
   });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -24,26 +25,38 @@ const Register: React.FC = () => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const emailPattern = /^[^\s@]+@illinois\.edu$/;
-    if (!emailPattern.test(formData.username)) {
-      alert('Please enter a valid @illinois.edu email address.');
+    if (!formData.firstName) {
+      alert("Please enter your first name.");
       return;
     }
-    const passwordPattern = /^(?=.*[A-Z].*[A-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])(?=.{10,})/;
+    if (!formData.lastName) {
+      alert("Please enter your last name.");
+      return;
+    }
+    const emailPattern = /^[^\s@]+@illinois\.edu$/;
+    if (!emailPattern.test(formData.username)) {
+      alert("Please enter a valid @illinois.edu email address.");
+      return;
+    }
+    const passwordPattern =
+      /^(?=.*[A-Z].*[A-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])(?=.{10,})/;
     if (!passwordPattern.test(formData.password)) {
-      alert('Please enter a password that is at least 10 characters long and contains at least 2 capital letters, at least 1 number, and at least 1 special character from the list: "!,@,#,$<%,&,*".');
+      alert(
+        'Please enter a password that is at least 10 characters long and contains at least 2 capital letters, at least 1 number, and at least 1 special character from the list: "!,@,#,$<%,&,*".'
+      );
       return;
     }
     if (formData.password !== formData.confirmPassword) {
-      alert('Passwords do not match.');
+      alert("Passwords do not match.");
       return;
     }
+
     try {
-      const response = await fetch('http://127.0.0.1:5000/register', {
-        method: 'POST',
-        mode: 'cors',
+      const response = await fetch("http://127.0.0.1:5000/register", {
+        method: "POST",
+        mode: "cors",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
@@ -52,7 +65,7 @@ const Register: React.FC = () => {
       alert(data.message);
     } catch (error) {
       console.error(error);
-      alert('An error occurred while registering. Please try again later.');
+      alert("An error occurred while registering. Please try again later.");
     }
   };
 
@@ -62,23 +75,53 @@ const Register: React.FC = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="firstName">First Name:</label>
-          <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} />
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="lastName">Last Name:</label>
-          <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange} />
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" value={formData.username} onChange={handleChange} />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="confirmPassword">Confirm Password:</label>
-          <input type="password" id="confirmPassword" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} />
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+          />
         </div>
         <button type="submit">Register</button>
       </form>
